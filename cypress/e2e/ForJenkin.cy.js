d63425e9be15cae7bb5ec15b5954c88576977250
HaiderAliRustam <hafizhaideraliuet@gmail.com>
2022-11-21 10:24:25 +0500

    pta ni kiya ho raha hay

--- cypress/e2e/ForJenkin.cy.js
+++ cypress/e2e/ForJenkin.cy.js
@@ -1,6 +1,11 @@
+
+
 //<refrence type ="cypress"/>
-describe("Test Jenkin ", ()=>{
-    it("This is for Jenkin",()=>{
+reporter: 'cypress-mochawesome-reporter',
+describe("Test Jenkin ", ()=>
+{
+    it("This is for Jenkin",()=>
+    {
         cy.visit("http://www.webdriveruniversity.com/")
         cy.get("#datepicker").invoke("removeAttr", "target").click()
         cy.url().should("contain", "http://www.webdriveruniversity.com/Datepicker/index.html")
