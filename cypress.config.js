b5827bd807a7dae33dc010d78e192a357bb923c0
Haider <113277775+HaiderAliRustam@users.noreply.github.com>
2023-01-24 13:59:37 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,12 @@
 const { defineConfig } = require("cypress");
 
-const cypressMultiReporters = require('cypress-multi-reporters');
-
-module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, args) => {
-    if (browser.name === 'chrome') {
-      args.push('--disable-gpu');
-      return args;
-    }
-  });
-  cypressMultiReporters.installPlugin(on, config);
-};
-
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   video: false,
   screenshotsFolder: false,
 
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "Haider-Report",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    showPassed: false,
-    saveAllAttempts: false,
-  },
+,
 
   e2e: {
     setupNodeEvents(on, config) {
@@ -33,10 +14,4 @@ module.exports = defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
 });
