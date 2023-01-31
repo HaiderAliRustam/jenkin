cee7100b985266df9517c88a014d0adb1244cf29
HaiderAliRustam <hafizhaideraliuet@gmail.com>
2023-01-31 10:53:48 +0500

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,15 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
+  //reporter: "cypress-mochawesome-reporter",
   video: false,
-  screenshotsFolder: false,
+  
 
 
 
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
+      //require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 
