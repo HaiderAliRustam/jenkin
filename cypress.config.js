d098eea6957a105b0deecec484e22e2710dce57a
Haider <113277775+HaiderAliRustam@users.noreply.github.com>
2023-01-31 10:57:02 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,24 +2,16 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
+  
   video: false,
   
 
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: "Haider-Report",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    showPassed: false,
-    saveAllAttempts: false,
-  },
 
  
 
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
+   
     },
   },
 
