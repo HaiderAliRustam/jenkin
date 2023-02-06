586e5dc5eb49d3c840df3597b5d67a940f10ec45
Haider <113277775+HaiderAliRustam@users.noreply.github.com>
2023-02-06 13:51:20 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,12 +11,9 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-<<<<<<< HEAD
+
       require("cypress-mochawesome-reporter/plugin")(on);
-      //haider
-=======
-   
->>>>>>> d098eea6957a105b0deecec484e22e2710dce57a
+
     },
   },
 
